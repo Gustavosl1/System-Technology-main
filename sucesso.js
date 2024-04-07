442c6bf28ecf9b3ec45a07559de0e1762b633e12
Gustavo Lima <gusouzalima5@gmail.com>
2024-04-07 15:50:15 -0300

    Commit -01

---
+++ sucesso.js
@@ -0,0 +1,4 @@
+// Adiciona um evento de clique ao botão "Voltar" para redirecionar à página anterior
+document.getElementById('backButton').addEventListener('click', function() {
+    window.history.back();
+});
